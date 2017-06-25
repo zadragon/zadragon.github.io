df4a91e2d44bca253984d3c10b5c5628a66c1c95
이자용 <yongs7786@gmail.com>
2017-06-25 22:24:38 +0900

    update

--- front/pre-build/js/ui.js
+++ front/pre-build/js/ui.js
@@ -44,5 +44,12 @@ function fe_toggle(i){
 }
 
 $(document).ready(function(){
+    $('.grid').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.grid-item',
+        // use element for option
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
     toggle.careerEvent();
 });
